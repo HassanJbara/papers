3bdc90f3b313e2aec1e1adb24fb36a1f9a8cb926
Hassan <hasan-account2012-3@live.com>
2023-09-17 13:35:47 +0200

    add wouter for routing, update app with routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
 import { PDFPage } from "@/pages";
 import { useEffect } from "react";
 import { MainPage } from "@/pages";
+import { Route } from "wouter";
 
 export function App() {
   useEffect(() => {
     document.title = "Papers";
   }, []);
 
-  return <MainPage />;
+  return (
+    <div>
+      <Route path="/" component={MainPage} />
+      <Route path="/pdf/:id">{(params) => <PDFPage pdfId={params.id} />}</Route>
+    </div>
+  );
 }
 
 export default App;
