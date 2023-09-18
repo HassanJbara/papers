663341e01901f893c9ae4fd6d3b46a32957fcac7
Hassan <hasan-account2012-3@live.com>
2023-09-18 19:12:12 +0200

    display current tags and categories in appropriate place, add ability to delete tags and categories

--- src/components/AddCategory.tsx
+++ src/components/AddCategory.tsx
@@ -2,9 +2,11 @@ import { useState } from "react";
 
 import { usePDFStore } from "@/stores";
 import { getNewId } from "@/utils";
+import { ReactSVG } from "react-svg";
 
 export function AddCategory() {
-  const { addCategory, resetCategories } = usePDFStore((state) => state);
+  const { addCategory, resetCategories, categories, removeCategory } =
+    usePDFStore((state) => state);
   const [categoryName, setCategoryName] = useState("");
   const changeCategoryName = (e: React.ChangeEvent<HTMLInputElement>) =>
     setCategoryName(e.target.value);
@@ -18,6 +20,33 @@ export function AddCategory() {
           className="input input-bordered w-full"
           onChange={changeCategoryName}
         />
+
+        <div className="mt-4 mb-2 text-xl font-bold text-base-content">
+          Current Categories:
+        </div>
+
+        <div className="flex flex-wrap gap-2">
+          {categories.map((category) => (
+            <div
+              className="flex flex-row gap-2 bg-info w-fit h-fit py-1 pr-3 items-center rounded-full"
+              key={category.id}
+            >
+              <button
+                className="btn btn-info btn-sm self-start btn-circle"
+                onClick={() => removeCategory(category.id)}
+              >
+                <ReactSVG
+                  src="/icons/x.svg"
+                  className="w-4 h-4 text-info-content fill-current"
+                />
+              </button>
+
+              <span className="text-info-content font-semibold">
+                {category.name}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
 
       <div className="modal-action flex flex-row w-full justify-around">
--- src/components/AddTag.tsx
+++ src/components/AddTag.tsx
@@ -2,9 +2,10 @@ import { useState } from "react";
 
 import { usePDFStore } from "@/stores";
 import { getNewId, getTagColor } from "@/utils";
+import { ReactSVG } from "react-svg";
 
 export function AddTag() {
-  const { addTag, resetTags } = usePDFStore((state) => state);
+  const { addTag, resetTags, removeTag, tags } = usePDFStore((state) => state);
   const [tagName, setTagName] = useState("");
   const changeTagName = (e: React.ChangeEvent<HTMLInputElement>) =>
     setTagName(e.target.value);
@@ -18,6 +19,33 @@ export function AddTag() {
           className="input input-bordered w-full"
           onChange={changeTagName}
         />
+
+        <div className="mt-4 mb-2 text-xl font-bold text-base-content">
+          Current Tags:
+        </div>
+
+        <div className="flex flex-wrap gap-2">
+          {tags.map((tag) => (
+            <div
+              className="flex flex-row gap-2 bg-info w-fit h-fit py-1 pr-3 items-center rounded-full"
+              key={tag.id}
+            >
+              <button
+                className="btn btn-info btn-sm self-start btn-circle"
+                onClick={() => removeTag(tag.id)}
+              >
+                <ReactSVG
+                  src="/icons/x.svg"
+                  className="w-4 h-4 text-info-content fill-current"
+                />
+              </button>
+
+              <span className="text-info-content font-semibold">
+                {tag.name}
+              </span>
+            </div>
+          ))}
+        </div>
       </div>
 
       <div className="modal-action flex flex-row w-full justify-around">
