0e28e573599ef94ffe1a43df6ebb5cc86aaec389
Hassan <hasan-account2012-3@live.com>
2023-09-20 19:41:40 +0200

    remove hash navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,7 @@
-import { PDFPage, MainPage } from "@/pages";
-
+import { PDFPage } from "@/pages";
 import { useEffect } from "react";
-import { Router, Route } from "wouter";
-import type { BaseLocationHook } from "wouter/use-location";
-import { useLocationProperty, navigate } from "wouter/use-location";
-
-// // returns the current hash location in a normalized form
-// // (excluding the leading '#' symbol)
-// const hashLocation = () => window.location.hash.replace(/^#/, "") || "/";
-
-// const hashNavigate = (to: any) => navigate("#" + to);
-
-// const useHashLocation: BaseLocationHook = () => {
-//   const location = useLocationProperty(hashLocation);
-//   return [location, hashNavigate];
-// };
+import { MainPage } from "@/pages";
+import { Route } from "wouter";
 
 export function App() {
   useEffect(() => {
@@ -22,10 +9,10 @@ export function App() {
   }, []);
 
   return (
-    <Router>
+    <div>
       <Route path="/" component={MainPage} />
       <Route path="/pdf/:id">{(params) => <PDFPage pdfId={params.id} />}</Route>
-    </Router>
+    </div>
   );
 }
 
